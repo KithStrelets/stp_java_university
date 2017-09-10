52f50d84617c01acfe14f1f3a89184160972da2f
KithStrelets <kith.strelets@gmail.com>
2017-09-10 19:41:59 +0300

    done lab 1

---
+++ Kpi_stp1/src/kpi_stp1/Kpi_stp1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kpi_stp1;
+
+/**
+ *
+ * @author Никита
+ */
+public class Kpi_stp1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello stp!");
+    }
+    
+}
